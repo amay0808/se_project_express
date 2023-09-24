5781f9884cf84f28c496002a4cd51691a423966f
Adrian Mayfield <amay0808@protonmail.com>
2023-09-24 06:55:46 -0700

    const JWT_SECRET = process.env.JWT_SECRET;

--- utils/config.js
+++ utils/config.js
@@ -4,6 +4,7 @@ const JWT_SECRET = process.env.JWT_SECRET || "default-secret-if-not-set";
 
 console.log("Loaded JWT_SECRET:", JWT_SECRET); // Log the loaded JWT_SECRET
 console.log("All Environment Variables:", process.env);
+console.log("JWT_SECRET:", process.env.JWT_SECRET); // Log JWT_SECRET environment variable
 
 module.exports = {
   JWT_SECRET,
