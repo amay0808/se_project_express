1019c09f2c9a585750eb960eef88047f2a654f8d
Adrian Mayfield <amay0808@protonmail.com>
2023-09-24 04:36:01 -0700

    console process.env in config.js

--- utils/config.js
+++ utils/config.js
@@ -3,6 +3,7 @@ require("dotenv").config(); // Load environment variables from .env file
 const JWT_SECRET = process.env.JWT_SECRET || "default-secret-if-not-set";
 
 console.log("Loaded JWT_SECRET:", JWT_SECRET); // Log the loaded JWT_SECRET
+console.log("All Environment Variables:", process.env);
 
 module.exports = {
   JWT_SECRET,
