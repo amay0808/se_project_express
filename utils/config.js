2350c365ed53b0a2615558889d950624613d527a
Adrian Mayfield <amay0808@protonmail.com>
2023-09-24 04:28:32 -0700

    added logs to config.js

--- utils/config.js
+++ utils/config.js
@@ -2,6 +2,8 @@ require("dotenv").config(); // Load environment variables from .env file
 
 const JWT_SECRET = process.env.JWT_SECRET || "default-secret-if-not-set";
 
+console.log("Loaded JWT_SECRET:", JWT_SECRET); // Log the loaded JWT_SECRET
+
 module.exports = {
   JWT_SECRET,
 };
