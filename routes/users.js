db587b37f2c40e5b8782b8006a758ff29cc1271d
Adrian Mayfield <amay0808@protonmail.com>
2023-09-27 05:33:19 -0700

    Added celebrate validation to updateCurrentUser route in users.js

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,10 @@
 const router = require("express").Router();
+const { validateUpdateCurrentUser } = require("../middlewares/validation"); // Import the new validation schema
 
-const {
-  getCurrentUser,
-  updateCurrentUser,
-  createUser,
-} = require("../controllers/users");
+const { getCurrentUser, updateCurrentUser } = require("../controllers/users");
 const auth = require("../middlewares/auth");
 
 router.get("/me", auth, getCurrentUser);
-router.patch("/me", auth, updateCurrentUser);
-router.post("/register", createUser);
-
-router.get("/:userId");
+router.patch("/me", auth, validateUpdateCurrentUser, updateCurrentUser); // Added the validation middleware
 
 module.exports = router;
