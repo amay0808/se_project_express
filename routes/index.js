c87b89aa7692453065b87d2181c1b40fadd5c70b
Adrian Mayfield <amay0808@protonmail.com>
2023-09-27 05:11:11 -0700

    delete auth from path correct error handling

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,8 @@ const router = require("express").Router();
 const userRoutes = require("./users");
 const clothingItemRoutes = require("./clothingItems");
 const unauthorizedRoutes = require("./unauthorized");
-const { NOT_FOUND } = require("../utils/errors");
 
-router.use("/auth", unauthorizedRoutes);
+router.use("/", unauthorizedRoutes);
 
 // Protected routes
 
@@ -12,8 +11,10 @@ router.use("/users", userRoutes);
 router.use("/items", clothingItemRoutes);
 
 // This handles all other undefined routes
-router.use((req, res) => {
-  res.status(NOT_FOUND).send({ message: "Route not found" });
+const NotFoundError = require("../errors/NotFoundError").NotFoundError;
+
+router.use(() => {
+  throw new NotFoundError("Route not found");
 });
 
 module.exports = router;
