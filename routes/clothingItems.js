afc92d41507e07544da1800953f598b3c86b0597
Adrian Mayfield <amay0808@protonmail.com>
2024-03-20 04:36:21 -0700

    reverse router.getItems in clothingitems.js

--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -13,9 +13,9 @@ const {
   getItemById,
 } = require("../controllers/clothingItems");
 
+router.get("/", getItems);
 router.get("/:id", getItemById);
 router.post("/", auth, validateClothingItem, createItem);
-router.get("/", getItems);
 router.delete("/:itemId", auth, validateId, deleteItem);
 router.put("/:itemId/likes", auth, validateId, likeItem); // Corrected the route to 'likes'
 router.delete("/:itemId/likes", auth, validateId, unlikeItem); // Corrected the route to 'likes'
